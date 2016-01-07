f923c5b12f329f068d7ea61af428292c0a2cca4e
jctraya <trayajc@gmail.com>
2016-01-07 04:02:13 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+from SimpleCV import Camera, Display
+#global x
+def main():
+
+    x = 0;
+    cam  = Camera()
+    disp = Display()
+    while disp.isNotDone():
+        img = cam.getImage()
+        img = img.scale(0.5)
+        faces = img.findHaarFeatures("eye.xml")
+        #print "not Detected"
+        if faces:
+            for face in faces:
+                face.draw()
+                print "eyes Detected"
+                x = 0
+        else:
+            
+                  x += 1
+                  print (x)    
+                  if x > 15:
+                    print "HOY GISING"
+
+                    return main()
+        img.show()
+
+
+
+ #if x > 15:
+ #print("Hoy gising")
+
+if __name__ == "__main__":
+    main()
